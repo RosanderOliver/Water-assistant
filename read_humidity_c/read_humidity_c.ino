2e7ef4a5776765dbce56f2cab073de441336e29b
olra13 <rosanderoliver@gmail.com>
2017-07-15 11:46:27 +0200

    Cleaned up the code

--- read_humidity_c/read_humidity_c.ino
+++ read_humidity_c/read_humidity_c.ino
@@ -1,23 +1,13 @@
-/*
-  AnalogReadSerial
-  Reads an analog input on pin 0, prints the result to the serial monitor.
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
- 
- This example code is in the public domain.
- */
 
-// the setup routine runs once when you press reset:
-
+// Define PIN slots
 #define PUMP1_PIN 13
 #define EARTH1_PIN A0
 
 void setup() {
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   pinMode(PUMP1_PIN, OUTPUT);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
   // read the resistance, lower is more humid
   int earth_humidity_1 = analogRead(EARTH1_PIN);
